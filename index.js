f081d7c6e5276882e79793ea4761ee39e7656117
RodentCoder <ichen080405@gmail.com>
2022-11-14 13:11:46 -0500

    ._.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const DOMselectors = {};
+const DOMselectors = {
+  Area: document.getElementsByClassName("Area"),
+  Button: document.getElementsByClassName("click"),
+  Name: document.querySelector("#name"),
+  Text: document.querySelector("#text"),
+  Image: document.querySelector("#image"),
+};
