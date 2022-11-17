27294c6192be244758b452671f5ebf4f67c9a0cc
RodentCoder <ichen080405@gmail.com>
2022-11-16 19:26:13 -0500

    HELP HELP

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const DOMselectors = {
-  Area: document.getElementsByClassName("Area"),
-  Button: document.getElementsByClassName("btn"),
+  Area: document.getElementsByClassName(".Area"),
+  Button: document.getElementsByClassName(".btn"),
   Name: document.querySelector("#name"),
-  Text: document.querySelector("#descript"),
+  Descript: document.querySelector("#descript"),
   Image: document.querySelector("#image"),
 };
 
-function texts(text) {
+function texts (text) {
   text.textcontent = "help";
 }
 
-DOMselectors.Button.addEventListener("click", function () {
-  texts(DOMselectors.Name);
+DOMselectors.Button.addEventListener("click", function () { 
+  texts (DOMselectors.Name);
 });
+  
+console.log(DOMselectors.Button)
