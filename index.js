a8302a51744363a8d4f2aad51e3ba608c57a74ad
RodentCoder <ichen080405@gmail.com>
2022-11-17 13:12:09 -0500

    time to burn it all down

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const DOMselectors = {
-  Area: document.getElementsByClassName(".Area"),
-  Button: document.getElementsByClassName(".btn"),
+  Area: document.getElementsByClassName(".area"),
+  Button: document.querySelector("#btn"),
   Name: document.querySelector("#name"),
   Descript: document.querySelector("#descript"),
   Image: document.querySelector("#image"),
 };
 
-function texts (text) {
+function texts(text) {
   text.textcontent = "help";
 }
 
-DOMselectors.Button.addEventListener("click", function () { 
-  texts (DOMselectors.Name);
+DOMselectors.Button.addEventListener("click", function () {
+  texts(DOMselectors.Name);
 });
-  
-console.log(DOMselectors.Button)
