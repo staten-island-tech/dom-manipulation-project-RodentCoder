0d40c2b0ccf21a5815047f09859ba4e71aab4a23
RodentCoder <ichen080405@gmail.com>
2022-11-21 19:12:37 -0500

    please work

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ const DOMselectors = {
   Name: document.getElementById("name"),
   Descript: document.getElementById("descript"),
   Image: document.getElementById("image"),
-  Result: document.getElementsByClassName("result"),
-  Reset: document.getElementById("reset"),
+  Card: document.getElementById("display"),
+  Remove: document.getElementById("remove")
 };
 
-function input(e) {
+DOMselectors.Button.addEventListener("click" , function(e){
   e.preventDefault();
-}
+
+  let Name = DOMselectors.Name.value;
+  let Descript = DOMselectors.Descript.value;
+
+  function createcard(){
+
+    DOMselectors.Card.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="card">
+      <h2 class="title">${Name.value}</h2>
+      <h3 class="para">${Descript.value}</h3>
+      <img class="img" src="${Image.value}"/>
+      </div>`
+    )
+  }
+
+  return createcard();
+ 
+  
+})
+  
+    
